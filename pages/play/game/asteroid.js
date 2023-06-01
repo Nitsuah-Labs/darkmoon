e331f2715f925383b203a7f2fdea7385dff8ee46
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2023-05-31 20:19:55 -0400

    Fix to compilation

--- pages/play/game/asteroid.js
+++ pages/play/game/asteroid.js
@@ -2,9 +2,6 @@ import { useState, useEffect, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Intersection } from 'three/src/core/Raycaster';
-import { Object3D } from 'three/src/core/Object3D';
-import { Mesh } from 'three/src/objects/Mesh';
 
 const Target = () => {
   const [hit, setHit] = useState(false);
@@ -15,24 +12,21 @@ const Target = () => {
     ref.current.rotation.y += 0.01;
   });
 
-  const handleClick = (e, intersects) => {
+  const handleClick = () => {
     // Check if the target was clicked
-    const target = intersects.find(intersect => intersect.object === ref.current);
-    if (target) {
-      setHit(true);
-    }
+    setHit(true);
   };
 
   return (
-    <Mesh ref={ref} onClick={handleClick}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <MeshStandardMaterial attach="material" color={hit ? 'red' : 'green'} />
-    </Mesh>
+    <mesh ref={ref} onClick={handleClick}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hit ? 'red' : 'green'} />
+    </mesh>
   );
 };
 
 const ShootingRange = () => {
-  const [gun, setGun] = useState();
+  const [gun, setGun] = useState(null);
 
   useEffect(() => {
     // Load the gun model
